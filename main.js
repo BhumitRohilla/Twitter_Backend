78dfb8334f26519703e3dcc7395474d87da81e66
BhumitRohilla <61738040+BhumitRohilla@users.noreply.github.com>
2023-05-02 18:47:03 +0530

    Incomplete Message

--- main.js
+++ main.js
@@ -1,48 +1,82 @@
-require('dotenv').config();
-const express = require('express');
+require("dotenv").config();
+const http = require("http");
+const express = require("express");
+const socketIo = require("socket.io");
 const app = express();
-const cors = require('cors');
-const cookieParser = require('cookie-parser');
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const server = http.createServer(app);
+const io = socketIo(server, {
+    cors: {
+        origin: "*",
+    },
+});
 
 //Middlewaer
 app.use(express.json());
 app.use(cookieParser());
-app.use(express.static('public'));
-app.use(express.static('Uploads'));
-app.use(cors({
-    origin:`http://${process.env.HOSTNAME}:${process.env.FRONTEND_PORT}`,
-    credentials: true
-}))
+app.use(express.static("public"));
+app.use(express.static("Uploads"));
+app.use(
+    cors({
+        origin: `http://${process.env.HOSTNAME}:${process.env.FRONTEND_PORT}`,
+        credentials: true,
+    })
+);
 
 //Router
-const auth = require('./Router/auth');
-const tweet = require('./Router/tweet');
-const user = require('./Router/user');
-const check = require('./Router/check');
+const auth = require("./Router/auth");
+const tweet = require("./Router/tweet");
+const user = require("./Router/user");
+const check = require("./Router/check");
 
 //Middleware
 
-const {authorize} = require('./Middleware/authrization');
+const { authorize } = require("./Middleware/authrization");
 
 ///(^|(?<=\s))@\w+/g;
 
-app.route('/testConnection')
-.get((req,res)=>{
-    res.send('Working');
-});
-
 //TODO:- Implement Signup function
 
-app.use('/auth',auth);
 
-app.use('/tweet',tweet);
+global.onlineUser = new Map();
 
-app.use('/user',authorize,user);
+io.on("connection", (socket) => {
+    socket.on('add-user',(data)=>{
+        console.log('user added',data,socket.id);
+        global.onlineUser.set(data,socket.id);
+    })
+    socket.on('disconnet',(data)=>{
+        console.log('user removed',data);
+        global.onlineUser.delete(data);
+    })
+    socket.on('send-message',({message,receiver,sender})=>{
+        let id = global.onlineUser.get(receiver);
+        console.log(message,receiver,sender,id);
+        if(id){
+            io.to(id).emit('recieve-message',{message,sender});
+        }
+    })
+});
 
-app.use('/check',check);
+app.use("/auth", auth);
 
-app.route('*').get((req,res)=>{res.status(404).json({err:'404 not found'});}).post((req,res)=>{res.status(404).json({err:'404 not found'});})
+app.use("/tweet", tweet);
 
-app.listen(process.env.BACKEND_PORT,process.env.HOSTNAME,()=>{
-    console.log(`server running at http://${process.env.HOSTNAME}:${process.env.BACKEND_PORT}`)
-})
+app.use("/user", authorize, user);
+
+app.use("/check", check);
+
+app.route("*")
+    .get((req, res) => {
+        res.status(404).json({ err: "404 not found" });
+    })
+    .post((req, res) => {
+        res.status(404).json({ err: "404 not found" });
+    });
+
+server.listen(process.env.BACKEND_PORT, process.env.HOSTNAME, () => {
+    console.log(
+        `server running at http://${process.env.HOSTNAME}:${process.env.BACKEND_PORT}`
+    );
+});
