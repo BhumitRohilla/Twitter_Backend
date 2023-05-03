f84c47b0ac1ecb3158c2c216ee8391cbe443162f
BhumitRohilla <61738040+BhumitRohilla@users.noreply.github.com>
2023-05-03 17:04:57 +0530

    Message Complete

--- Services/database/message.js
+++ Services/database/message.js
@@ -12,7 +12,14 @@ function insertMessage(sender,receiver,message){
 }
 
 function getUserToConvo(u_id){
-    let queury = `select u_id,username,profilepicture,email,name from users where u_id in (select distinct u_id from (select sender as u_id from messages where receiever = ${u_id} union select receiever as u_id from messages where sender = ${u_id}) as usersToShow);`;
+    let queury = `select users.u_id,users.name,users.username,users.profilepicture,users.email from (
+        select max(timeofpost) as timeofpost ,u_id from (	
+        select max(timeofpost) as timeofpost, sender as u_id from messages group by (sender,receiever) having receiever = ${u_id}
+        union
+        select max(timeofpost) as timeofpost, receiever as u_id from messages group by (sender,receiever) having sender= ${u_id}
+            ) as myTable
+        group by u_id
+            ) as myTable inner join users on myTable.u_id = users.u_id order by timeofpost desc;`;
     return execQueury(queury);
 }
 
